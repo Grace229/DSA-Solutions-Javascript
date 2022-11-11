4ceb930a119d02ca5e1fe80ea1f9d6446c4122a1
Grace229 <itiunggrace@gmail.com>
2022-11-11 12:12:54 +0100

    Leet Code Remove Duplicate from Sorted array

--- index.js
+++ index.js
@@ -141,3 +141,21 @@ var threeSum = function(nums) {
   }
   return results
 };
+// Leet Code Remove Duplicate from Sorted array
+var removeDuplicates = function (nums) {
+  const numsLength = nums.length;
+
+  if (numsLength === 0) {
+    return 0;
+  }
+
+  let j = 1;
+  for (let i = 0; i < numsLength - 1; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      nums[j] = nums[i + 1];
+      j++;
+    }
+  }
+
+  return j;
+};
