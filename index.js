1314a8e4b2f3a5b3ee0e7b0058e567baa7f7be1f
Grace229 <itiunggrace@gmail.com>
2022-11-11 12:31:12 +0100

    Best Time to buy stock LeetCode

--- index.js
+++ index.js
@@ -159,3 +159,15 @@ var removeDuplicates = function (nums) {
 
   return j;
 };
+// Best Time to buy stock LeetCode
+var maxProfit = function(prices) {
+  let minBuyPrice = prices[0]
+  let max = 0
+   for(let i = 1; i < prices.length; i++){
+       const sellPrice = prices[i]
+       const profit = sellPrice-minBuyPrice
+       max=Math.max(max, profit)
+       minBuyPrice=Math.min(minBuyPrice, prices[i])
+   }
+   return max
+};
